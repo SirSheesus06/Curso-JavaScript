7c91681f678df6f733629cb670cb4a44b4d0bd3c
SirSheesus06 <toot.sheesus.ram@gmail.com>
2023-03-23 16:03:36 -0300

    new

---
+++ 01.Primer-Proyecto-Node/index.js
@@ -0,0 +1,8 @@
+// Este es el mensaje de bienvenida
+
+console.log("Esta es la puerta de entrada al proyecto");
+
+
+// Este es el mensaje final del proyecto
+
+console.log("Aqui termina el proyecto");
