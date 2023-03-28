013db212b521419b2f0576d9019a205362702a9a
SirSheesus06 <toot.sheesus.ram@gmail.com>
2023-03-28 16:46:23 -0300

    strings

---
+++ 04-Strings/Introduccion-strings.js
@@ -0,0 +1,35 @@
+// Strings
+
+let str_simp = 'Esto es un string con caracteres simples';
+let str_dobl = 'Esto es un string con caracteres dobles';
+
+console.log(str_simp);
+console.log(str_dobl);
+
+// Texto con comillas
+
+let str_comillas = "Es solo un \"chiste\"";
+let str_comillas_simples = 'Es solo un "chiste"';
+let str_comillas_dobles = "Es solo un 'chiste'";
+
+console.log(str_comillas);
+console.log(str_comillas_simples);
+console.log(str_comillas_dobles);
+
+// Texto con comillas invertidas (backticks)
+
+let nombre = "Juan Manuel";
+let str_backticks = `Hola, ${nombre}`;
+
+console.log(str_backticks)
+
+// Plantilla HTML
+
+let plantilla = `
+    <html>
+        <h1>Pagina web de ${nombre}</h1>
+        <p>Esto es un parrafo</p>
+    </html>
+`
+
+console.log(plantilla);
