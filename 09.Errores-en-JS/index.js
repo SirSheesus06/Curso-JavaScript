c1c85042e4eb94f3c5c00927a0a9770384a8b8ba
juanmcr06 <juanmcr.06@gmail.com>
2023-04-26 03:56:15 -0300

    errores

---
+++ 09.Errores-en-JS/index.js
@@ -0,0 +1,9 @@
+const logger = require('./looger')
+
+// Los console. mas utilizados
+
+// console.log("Esto sale por pantalla")
+logger.info("Esto es un mensaje de infromacion por consola")
+logger.debug("Esto es un mensaje de debug por consola")
+logger.warn("Esto es un mensaje de advertencia por consola")
+logger.error("Esto es un mensaje de error por consola")
