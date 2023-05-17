d819bd4177a14b9d3c63a3a595614ef06362d1c3
Juanma <juanmcr.06@gmail.com>
2023-05-17 16:12:52 -0300

    jquery

--- 14.HTML-JS/JQuery/index.js
+++ 14.HTML-JS/JQuery/index.js
@@ -11,10 +11,27 @@ $(() => {
     //$("#el-1").hide()
     $(".hide-btn").click(() => {
         console.log("Ocultando...")
-        $("h1").hide()
+        //$("h1").hide()
+        $("h1").fadeOut()
     })
     $(".show-btn").click(() => {
         console.log("Mostrando...")
-        $("h1").show()
+        //$("h1").show()
+        $("h1").fadeIn()
+    })
+
+    $(".show-btn").click(()=> {
+        console.log("Cambio de color");
+        $(".ti-color").css({color: "red"})
+    })
+
+    $(".pre-element").click(()=> {
+        console.log("Añadiendo objeto al inicio");
+        $("ul").prepend("<li>New Element</li>")
+    })
+
+    $(".last-element").click(()=> {
+        console.log("Añadiendo objeto al iniciofinal");
+        $("ul").append("<li>New Element</li>")
     })
 })
